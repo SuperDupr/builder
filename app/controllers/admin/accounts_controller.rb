eb00b39f92e01e110615fdbc6ba17a8a0b96b998
Moiz Ali <amoiz4142@gmail.com>
2023-07-13 18:01:58 +0500

    Fix whitespaces

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -1,5 +1,5 @@
 # In our app this controller serves the purpose of organizations controller
-# Being wrapped in the admin scope, an Organization of FTE is equivalent of 
+# Being wrapped in the admin scope, an Organization of FTE is equivalent of
 # Account in Jumpstart
 
 module Admin
@@ -8,20 +8,20 @@ module Admin
     # For example, you may want to send an email after a foo is updated.
     #
     def create
-      super      
+      super
       flash[:notice] = "Organization was created successfully!"
     end
-    
+
     def update
       super
-      flash[:notice] = "Organization was updated successfully!"      
+      flash[:notice] = "Organization was updated successfully!"
     end
 
     def destroy
       super
-      flash[:notice] = "Organization was deleted successfully!"  
+      flash[:notice] = "Organization was deleted successfully!"
     end
-    
+
     # Override this method to specify custom lookup behavior.
     # This will be used to set the resource for the `show`, `edit`, and `update`
     # actions.
