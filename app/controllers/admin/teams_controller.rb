6f94eecf51bbcd43ad15e1ece26650402b4b36ba
gullu143 <chwaqaspunjabian143@gmail.com>
2023-07-18 17:42:09 +0500

    lates commit pick from git hub

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -16,21 +16,36 @@ module Admin
 
     def create
       @team = @account.teams.new(team_params)
+
+      @team.save ?
+        flash[:notice] = "Team was created successfully!" :
+        flash[:alert] = "Unable to create team. Errors: #{@team.errors.full_messages.join(", ")}"
+      
+      redirect_to(admin_teams_path(account_id: @account.id))
     end
 
-    def edit 
-    end
+    def edit; end
 
-    def update
+    def update      
+      @team.update(team_params) ?
+        flash[:notice] = "Team was updated successfully!" :
+        flash[:alert] = "Unable to update team. Errors: #{@team.errors.full_messages.join(", ")}"
+      
+      redirect_to(admin_teams_path(account_id: @account.id))
     end
 
     def destroy
+      @team.destroy ?
+        flash[:notice] = "Team was deleted successfully!" :
+        flash[:alert] = "Unable to delete team!"
+      
+      redirect_to(admin_teams_path(account_id: @account.id))
     end
 
     private
 
     def set_account
-      @account = Account.find(params[:account_id])      
+      @account = Account.find(params[:account_id] || params[:team][:account_id])      
     end
 
     def set_team
@@ -38,7 +53,7 @@ module Admin
     end
 
     def team_params
-      params.require(:team).permit(:name)      
+      params.require(:team).permit(:name, :account_id)      
     end
   end  
 end
