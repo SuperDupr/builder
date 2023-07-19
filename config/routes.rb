2f46af6a2d61fe3b4fdfe10032bcfa9a4df66b06
gullu143 <chwaqaspunjabian143@gmail.com>
2023-07-19 18:47:23 +0500

    add new user form

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
         member do
           get :organization_users, as: :organization_users
           patch :manage_access, as: :manage_access
+          get :adduser, as: :adduser
         end
       end
       resources :account_users
