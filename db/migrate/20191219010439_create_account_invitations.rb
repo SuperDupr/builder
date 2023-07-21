bbc2c79b4c7a320beae43c7e4283823d7c517269
Moiz Ali <amoiz4142@gmail.com>
2023-07-21 21:06:26 +0500

    Add columns inside migrations

--- db/migrate/20191219010439_create_account_invitations.rb
+++ db/migrate/20191219010439_create_account_invitations.rb
@@ -4,8 +4,11 @@ class CreateAccountInvitations < ActiveRecord::Migration[6.0]
       t.belongs_to :account, null: false, foreign_key: true
       t.belongs_to :invited_by, null: false, foreign_key: {to_table: :users}
       t.string :token, null: false
-      t.string :name, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :email, null: false
+      t.string :team_name
+      t.integer :team_id
       t.jsonb :roles, null: false, default: {}
 
       t.timestamps
