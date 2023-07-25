dffba0da2bac18cdfe1572c1dea9ff023ea16336
Moiz Ali <amoiz4142@gmail.com>
2023-07-25 17:43:31 +0500

    Add teams controller

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,59 @@
+class TeamsController < Accounts::BaseController
+  before_action :authenticate_user!
+  before_action :set_account
+  before_action :require_account_admin
+  before_action :set_team, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @teams = @account.teams
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = @account.teams.new(team_params)
+
+    @team.save ?
+      flash[:notice] = "Team was created successfully!" :
+      flash[:alert] = "Unable to create team. Errors: #{@team.errors.full_messages.join(", ")}"
+
+    redirect_to(teams_path)
+  end
+
+  def edit
+  end
+
+  def update
+    @team.update(team_params) ?
+      flash[:notice] = "Team was updated successfully!" :
+      flash[:alert] = "Unable to update team. Errors: #{@team.errors.full_messages.join(", ")}"
+
+    redirect_to(teams_path)
+  end
+
+  def show    
+  end
+
+  def destroy
+    @team.destroy ?
+      flash[:notice] = "Team was deleted successfully!" :
+      flash[:alert] = "Unable to delete team!"
+    redirect_to(teams_path)
+  end
+
+  private
+
+  def set_account
+    @account = current_account
+  end
+
+  def set_team
+    @team = Team.find(params[:id])    
+  end
+
+  def team_params
+    params.require(:team).permit(:name)
+  end
+end
