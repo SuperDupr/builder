2dfa647820ce9b8b108e7dd50fcdb42109e9b7f1
Moiz Ali <amoiz4142@gmail.com>
2023-07-26 17:40:50 +0500

    Add follow-up email for account setup

---
+++ app/mailers/follow_ups_mailer.rb
@@ -0,0 +1,12 @@
+class FollowUpsMailer < ApplicationMailer
+  def account_owner_setup(account, temp_password)
+    @account = account
+    @temp_password = temp_password
+
+    mail(
+      to: @account.owner.email,
+      from: email_address_with_name(Jumpstart.config.support_email, "Feed The Elephant - Team"),
+      subject: "Admin Account setup for Organization: - #{@account.name}"
+    )   
+  end
+end
