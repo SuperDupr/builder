59621377b50da4bdff7d428d5e7ab12de31a679b
Moiz Ali <amoiz4142@gmail.com>
2023-07-31 18:39:52 +0500

    Add Teams test suite

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -19,7 +19,24 @@
 require "test_helper"
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @associations_reflector = Team.reflect_on_all_associations
+  end
+
+  test "Associations" do
+    assoc_classes = @associations_reflector.map { |assoc_ref| assoc_ref.class }
+    assoc_names = @associations_reflector.map { |assoc_ref| assoc_ref.name }
+    
+    assert_includes(assoc_classes, ActiveRecord::Reflection::BelongsToReflection)
+    assert_includes(assoc_names, :account)
+
+    assert_includes(assoc_classes, ActiveRecord::Reflection::HasManyReflection)
+    assert_includes(assoc_names, :users)
+  end
+
+  test "Validations" do
+    team = Team.new(name: "")
+
+    assert(team.invalid?, true)
+  end
 end
