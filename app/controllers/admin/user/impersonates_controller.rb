3e72f648e2d30c60a171292d88f46244d1af0528
Moiz Ali <amoiz4142@gmail.com>
2023-08-03 16:01:46 +0500

    Apply standardrb --fix

--- app/controllers/admin/user/impersonates_controller.rb
+++ app/controllers/admin/user/impersonates_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class User::ImpersonatesController < Admin::ApplicationController
     before_action :set_account, only: [:destroy]
-    
+
     def create
       user = ::User.find(params[:user_id])
       impersonate_user(user)
@@ -9,13 +9,12 @@ module Admin
     end
 
     def destroy
-      user = current_user
       stop_impersonating_user
       redirect_to(organization_users_admin_account_path(@account))
     end
-    
+
     private
-    
+
     def set_account
       @account = Account.find(params[:account_id])
     end
