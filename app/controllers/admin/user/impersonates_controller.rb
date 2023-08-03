fc2802cfb8964b5d2557897b2b36e55a757f673e
Moiz Ali <amoiz4142@gmail.com>
2023-08-03 15:59:38 +0500

    Add backend for masquerading

--- app/controllers/admin/user/impersonates_controller.rb
+++ app/controllers/admin/user/impersonates_controller.rb
@@ -1,5 +1,7 @@
 module Admin
   class User::ImpersonatesController < Admin::ApplicationController
+    before_action :set_account, only: [:destroy]
+    
     def create
       user = ::User.find(params[:user_id])
       impersonate_user(user)
@@ -9,7 +11,13 @@ module Admin
     def destroy
       user = current_user
       stop_impersonating_user
-      redirect_to admin_user_path(user)
+      redirect_to(organization_users_admin_account_path(@account))
+    end
+    
+    private
+    
+    def set_account
+      @account = Account.find(params[:account_id])
     end
   end
 end
