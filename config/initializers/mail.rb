971e89c561237fe652ba94ae81901b27cf950e49
Spencer Norman <spencerdnorman@gmail.com>
2023-08-18 09:28:29 -0500

    Update default url (#75)
    * updated the staging and production env default urls for action mailer
    
    * lint fix

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,12 @@
 Rails.application.reloader.to_prepare do
   ActionMailer::Base.default_options = {from: Jumpstart.config.default_from_email}
 
-  if Rails.env.production? || Rails.env.staging?
-    ActionMailer::Base.default_url_options[:host] = Jumpstart.config.domain
+  if Rails.env.production? # || Rails.env.staging?
+    ActionMailer::Base.default_url_options[:host] = "app.feedtheelephant.com"
+    ActionMailer::Base.default_url_options[:protocol] = "https"
+    ActionMailer::Base.smtp_settings.merge!(Jumpstart::Mailer.new(Jumpstart.config).settings)
+  elsif Rails.env.staging?
+    ActionMailer::Base.default_url_options[:host] = "staging.feedtheelephant.com"
     ActionMailer::Base.default_url_options[:protocol] = "https"
     ActionMailer::Base.smtp_settings.merge!(Jumpstart::Mailer.new(Jumpstart.config).settings)
   end
