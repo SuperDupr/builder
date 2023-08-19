c51e35320aa76858fb57dc34f6c3771c8bb151ae
Moiz Ali <amoiz4142@gmail.com>
2023-08-19 22:41:49 +0500

    Annotated changes

--- test/models/node_test.rb
+++ test/models/node_test.rb
@@ -2,10 +2,12 @@
 #
 # Table name: nodes
 #
-#  id         :bigint           not null, primary key
-#  title      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id             :bigint           not null, primary key
+#  title          :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  parent_node_id :integer
+#  question_id    :integer
 #
 require "test_helper"
 
