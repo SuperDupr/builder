9a7d187232e9e5005c70bac144f7963b7bf2d195
Moiz Ali <amoiz4142@gmail.com>
2023-08-20 00:18:53 +0500

    Add logic to attach questions with builder

--- app/controllers/admin/story_builders_controller.rb
+++ app/controllers/admin/story_builders_controller.rb
@@ -7,12 +7,15 @@ class Admin::StoryBuildersController < Admin::ApplicationController
 
   def new
     @story_builder = StoryBuilder.new
+    @pagy, @questions = pagy(Question.all, items: 30)
   end
 
   def create
     @story_builder = StoryBuilder.new(story_builder_params)
 
     if @story_builder.save
+      questionnaire_data = params[:builder][:q_ids].compact_blank.map { |id| { question_id: id, story_builder_id: @story_builder.id } }
+      Questionnaire.insert_all(questionnaire_data) if questionnaire_data.present?
       redirect_to(admin_story_builders_path, notice: "Builder created successfully!")
     else
       redirect_to(admin_story_builders_path,
@@ -24,6 +27,7 @@ class Admin::StoryBuildersController < Admin::ApplicationController
   end
 
   def edit
+    @pagy, @questions = pagy(Question.all, items: 30)
   end
 
   def update
@@ -45,7 +49,7 @@ class Admin::StoryBuildersController < Admin::ApplicationController
   private
 
   def story_builder_params
-    params.require(:story_builder).permit(:title)
+    params.require(:story_builder).permit(:title, q_ids: [])
   end
 
   def set_story_builder
