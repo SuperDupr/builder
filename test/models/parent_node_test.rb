b58908f07076d0edce518b3cec845c7223774338
Moiz Ali <amoiz4142@gmail.com>
2023-08-20 23:40:50 +0500

    Add ParentNode test suite

--- test/models/parent_node_test.rb
+++ test/models/parent_node_test.rb
@@ -12,7 +12,25 @@
 require "test_helper"
 
 class ParentNodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @parent_node = ParentNode.create!(title: "A parent node!", question: questions(:one))
+  end
+
+  test "should inherit from Node class" do
+    assert_includes(ParentNode.ancestors, Node)
+  end
+
+  test "belongs_to question association" do
+    assert_respond_to @parent_node, :question
+    assert_instance_of Question, @parent_node.question
+  end
+
+  test "has_many child_nodes association" do
+    assert_respond_to @parent_node, :child_nodes
+    assert_instance_of ChildNode, @parent_node.child_nodes.build
+  end
+
+  test "accepts_nested_attributes_for child_nodes" do
+    assert_respond_to @parent_node, :child_nodes_attributes=
+  end
 end
