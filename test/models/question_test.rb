416c2116c5cf54957e9e6033d4f8f5634d7c7d30
Moiz Ali <amoiz4142@gmail.com>
2023-08-20 23:28:49 +0500

    Add Question tests suite

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -10,7 +10,40 @@
 require "test_helper"
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @question = questions(:one)
+  end
+
+  test "has_many prompts association" do
+    assert_respond_to @question, :prompts
+    assert_instance_of Prompt, @question.prompts.build
+  end
+
+  test "has_many parent_nodes association" do
+    assert_respond_to @question, :parent_nodes
+    assert_instance_of ParentNode, @question.parent_nodes.build
+  end
+
+  test "has_one answer association" do
+    assert_respond_to @question, :answer
+    assert_instance_of Answer, @question.build_answer
+  end
+
+  test "has_many questionnaires association" do
+    assert_respond_to @question, :questionnaires
+    assert_instance_of Questionnaire, @question.questionnaires.build
+  end
+
+  test "has_many story_builders through questionnaires association" do
+    assert_respond_to @question, :story_builders
+    assert_instance_of StoryBuilder, @question.story_builders.build
+  end
+
+  test "accepts_nested_attributes_for prompts" do
+    assert_respond_to @question, :prompts_attributes=
+  end
+
+  test "accepts_nested_attributes_for parent_nodes" do
+    assert_respond_to @question, :parent_nodes_attributes=
+  end
 end
