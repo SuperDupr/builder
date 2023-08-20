93b268d8449bae495ad7ed4a5ca4d428be5bbd4d
Moiz Ali <amoiz4142@gmail.com>
2023-08-20 23:43:53 +0500

    Add ChildNode test suite

--- test/models/child_node_test.rb
+++ test/models/child_node_test.rb
@@ -12,7 +12,13 @@
 require "test_helper"
 
 class ChildNodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    parent_node = ParentNode.create!(title: "A parent node!", question: questions(:one))
+    @child_node = parent_node.child_nodes.create!(title: "A Child Node")
+  end
+
+  test "belongs_to parent_node association" do
+    assert_respond_to @child_node, :parent_node
+    assert_instance_of ParentNode, @child_node.parent_node
+  end
 end
