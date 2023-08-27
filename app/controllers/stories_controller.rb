f918002ee29171fb9a0d9893cccdebab0c7f70a5
Moiz Ali <amoiz4142@gmail.com>
2023-08-28 00:17:53 +0500

    Add stories#index page for organization

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,6 +2,9 @@ class StoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_story, only: [:show, :edit, :update, :destroy]
 
+  def index
+  end
+  
   def new
     @story = Story.new
   end
