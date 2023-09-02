678513b86cc09cdd53c4c9e43e67998de008b505
Moiz Ali <amoiz4142@gmail.com>
2023-09-02 14:45:28 +0500

    Manage fallback path when registration questions are not answered

--- app/controllers/users/registration_questions_controller.rb
+++ app/controllers/users/registration_questions_controller.rb
@@ -2,12 +2,24 @@ class Users::RegistrationQuestionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    if params[:fallback] == "story_creation"
+      flash[:alert] = "Please answer some basic questions to lead up the story building process!"
+    end
+    
     @industries = current_account.industries.map { |industry| [industry.title, industry.id] }
   end
 
   def update_data
+    if params[:fallback] == "story_creation"
+      fallback_path = story_builders_path
+      notice = "Click Start Building link against the builder to get started with your story."
+    else
+      fallback_path = registration_questions_path
+      notice = "Profile updated successfully!"
+    end
+
     if current_user.update(registration_params)
-      redirect_to(registration_questions_path, notice: "Profile updated successfully!")
+      redirect_to(fallback_path, notice: notice)
     else
       redirect_to(registration_questions_path, alert: current_user.errors.full_messages.join(", "))
     end
