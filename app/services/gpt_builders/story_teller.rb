b4025f867575f07c9749abc73efaf6ba383e9711
Moiz Ali <amoiz4142@gmail.com>
2023-10-19 11:28:38 +0500

    Start implementation of gpt-4 service

---
+++ app/services/gpt_builders/story_teller.rb
@@ -0,0 +1,24 @@
+module GptBuilders
+  class StoryTeller < ApplicationService
+    def initialize(options={})
+      @model = options[:model]
+      @temperature = options[:temperature] || "some_default_temperature_value"
+      @raw_data = options[:raw_data]
+    end
+
+    def call
+      sort_data_for_ai
+      feed_data_to_ai
+    end
+
+    private
+
+    def sort_data_for_ai
+      raise NotImplementedError
+    end
+
+    def feed_data_to_ai
+      raise NotImplementedError
+    end
+  end
+end
