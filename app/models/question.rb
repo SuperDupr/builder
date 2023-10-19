7e07db18f1d81f44e100e497952a0358d18db296
Moiz Ali <amoiz4142@gmail.com>
2023-10-19 11:28:06 +0500

    Add method for preparing conversational data, feed to AI

--- app/models/question.rb
+++ app/models/question.rb
@@ -18,4 +18,17 @@ class Question < ApplicationRecord
 
   accepts_nested_attributes_for :prompts, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :parent_nodes, reject_if: :all_blank, allow_destroy: true
+
+  # Class methods
+  def self.questionnaires_conversational_data(story_id:)
+    joins("LEFT JOIN answers ON questions.id = answers.question_id AND answers.story_id = #{story_id} 
+      LEFT JOIN prompts ON questions.id = prompts.question_id")
+      .pluck(
+        'questions.title',
+        'answers.id',
+        'answers.response',
+        Arel.sql("COALESCE(prompts.pre_text, '') as pre_text"),
+        Arel.sql("COALESCE(prompts.post_text, '') as post_text")
+      )
+  end
 end
