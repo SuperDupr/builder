fd8b732beba7b221cc7583d2ead2754e0eaffac1
Moiz Ali <amoiz4142@gmail.com>
2023-10-23 12:32:56 +0500

    Add :ai_generated_content column in Stories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_16_112757) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_23_060623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -347,6 +347,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_112757) do
     t.bigint "account_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "ai_generated_content"
     t.index ["account_id"], name: "index_stories_on_account_id"
     t.index ["creator_id"], name: "index_stories_on_creator_id"
     t.index ["story_builder_id"], name: "index_stories_on_story_builder_id"
