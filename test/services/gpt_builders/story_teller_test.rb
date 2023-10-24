37ee8cd75d9dcae5e1de99e92f95fdd48b43d8df
Moiz Ali <amoiz4142@gmail.com>
2023-10-24 19:00:09 +0500

    Lint fixes

--- test/services/gpt_builders/story_teller_test.rb
+++ test/services/gpt_builders/story_teller_test.rb
@@ -15,20 +15,21 @@ class StoryTellerTest < ActiveSupport::TestCase
   end
 
   def stubbed_ai_request(stubbed_response)
-    stub_request(:post, "https://api.openai.com/v1/chat/completions").
-    with(
-      body: "{\"model\":\"gpt-3.5-turbo\",\"messages\":[{\"role\":\"system\",\"content\":\"Hey Chat!. User specific instructions to supersede: Nothing specific\"},{\"role\":\"user\",\"content\":\"Here is the conversational responses data received from user: {\\\"key1\\\"=\\u003e[{\\\"id\\\"=\\u003e1, \\\"response\\\"=\\u003e\\\"response1\\\", \\\"prompt\\\"=\\u003e{\\\"pre_text\\\"=\\u003e\\\"pre1\\\", \\\"post_text\\\"=\\u003e\\\"post1\\\"}}, {\\\"id\\\"=\\u003e2, \\\"response\\\"=\\u003e\\\"response2\\\"}], \\\"key2\\\"=\\u003e[{\\\"id\\\"=\\u003e3, \\\"response\\\"=\\u003e\\\"response3\\\", \\\"prompt\\\"=\\u003e{\\\"pre_text\\\"=\\u003e\\\"pre3\\\", \\\"post_text\\\"=\\u003e\\\"post3\\\"}}]}\"}],\"temperature\":0.2}",
-      headers: {
-        'Accept'=>'*/*',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Content-Type'=>'application/json',
-        'User-Agent'=>'Ruby'
-      }).
-    to_return(status: 200, body: {
-      "choices": [
-        { "message": { "content": stubbed_response }}
-      ]  
-    }.to_json, headers: {})
+    stub_request(:post, "https://api.openai.com/v1/chat/completions")
+      .with(
+        body: "{\"model\":\"gpt-3.5-turbo\",\"messages\":[{\"role\":\"system\",\"content\":\"Hey Chat!. User specific instructions to supersede: Nothing specific\"},{\"role\":\"user\",\"content\":\"Here is the conversational responses data received from user: {\\\"key1\\\"=\\u003e[{\\\"id\\\"=\\u003e1, \\\"response\\\"=\\u003e\\\"response1\\\", \\\"prompt\\\"=\\u003e{\\\"pre_text\\\"=\\u003e\\\"pre1\\\", \\\"post_text\\\"=\\u003e\\\"post1\\\"}}, {\\\"id\\\"=\\u003e2, \\\"response\\\"=\\u003e\\\"response2\\\"}], \\\"key2\\\"=\\u003e[{\\\"id\\\"=\\u003e3, \\\"response\\\"=\\u003e\\\"response3\\\", \\\"prompt\\\"=\\u003e{\\\"pre_text\\\"=\\u003e\\\"pre3\\\", \\\"post_text\\\"=\\u003e\\\"post3\\\"}}]}\"}],\"temperature\":0.2}",
+        headers: {
+          "Accept" => "*/*",
+          "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+          "Content-Type" => "application/json",
+          "User-Agent" => "Ruby"
+        }
+      )
+      .to_return(status: 200, body: {
+        choices: [
+          {message: {content: stubbed_response}}
+        ]
+      }.to_json, headers: {})
 
     stubbed_response
   end
@@ -47,8 +48,8 @@ class StoryTellerTest < ActiveSupport::TestCase
     story_teller = GptBuilders::StoryTeller.new(@options)
 
     assert_equal(
-      story_teller.send(:finalized_system_ai_prompt), 
-      "#{@options[:system_ai_prompt]}. User specific instructions to supersede: #{@options[:admin_ai_prompt]}" 
+      story_teller.send(:finalized_system_ai_prompt),
+      "#{@options[:system_ai_prompt]}. User specific instructions to supersede: #{@options[:admin_ai_prompt]}"
     )
   end
 
@@ -58,7 +59,7 @@ class StoryTellerTest < ActiveSupport::TestCase
 
     assert_equal(
       story_teller.send(:finalized_data_feed),
-      "Here is the conversational responses data received from user: #{story_teller.instance_variable_get(:@data)}" 
+      "Here is the conversational responses data received from user: #{story_teller.instance_variable_get(:@data)}"
     )
   end
 
@@ -77,10 +78,10 @@ class StoryTellerTest < ActiveSupport::TestCase
   test "data sorting functionality" do
     story_teller = GptBuilders::StoryTeller.new(@options)
     data = story_teller.instance_variable_get(:@data)
-    
+
     assert_includes(data.keys, "key1")
     assert_includes(data.keys, "key2")
-    
+
     key1_data = data["key1"]
     assert_equal 2, key1_data.length
   end
