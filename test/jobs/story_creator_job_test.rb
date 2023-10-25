3ec65dacc6af3e2a453fb93c2b68d99590dff28a
Moiz Ali <amoiz4142@gmail.com>
2023-10-25 16:20:30 +0500

    Lint fixes

--- test/jobs/story_creator_job_test.rb
+++ test/jobs/story_creator_job_test.rb
@@ -10,20 +10,21 @@ class StoryCreatorJobTest < ActiveJob::TestCase
   end
 
   def stubbed_ai_request(stubbed_response)
-    stub_request(:post, "https://api.openai.com/v1/chat/completions").
-    with(
-      body: "{\"model\":\"gpt-3.5-turbo\",\"messages\":[{\"role\":\"system\",\"content\":\"Nothing special!\"},{\"role\":\"user\",\"content\":\"Here is the conversational responses data received from user: {}\"}],\"temperature\":0.2}",
-      headers: {
-        'Accept'=>'*/*',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Content-Type'=>'application/json',
-        'User-Agent'=>'Ruby'
-      }).
-    to_return(status: 200, body: {
-      choices: [
-        {message: {content: stubbed_response}}
-      ]
-    }.to_json, headers: {})
+    stub_request(:post, "https://api.openai.com/v1/chat/completions")
+      .with(
+        body: "{\"model\":\"gpt-3.5-turbo\",\"messages\":[{\"role\":\"system\",\"content\":\"Nothing special!\"},{\"role\":\"user\",\"content\":\"Here is the conversational responses data received from user: {}\"}],\"temperature\":0.2}",
+        headers: {
+          "Accept" => "*/*",
+          "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+          "Content-Type" => "application/json",
+          "User-Agent" => "Ruby"
+        }
+      )
+      .to_return(status: 200, body: {
+        choices: [
+          {message: {content: stubbed_response}}
+        ]
+      }.to_json, headers: {})
 
     stubbed_response
   end
--- test/services/gpt_builders/story_teller_test.rb
+++ test/services/gpt_builders/story_teller_test.rb
@@ -14,16 +14,17 @@ class StoryTellerTest < ActiveSupport::TestCase
   end
 
   def stubbed_ai_request(stubbed_response)
-    stub_request(:post, "https://api.openai.com/v1/chat/completions").
-    with(
-      body: "{\"model\":\"gpt-3.5-turbo\",\"messages\":[{\"role\":\"system\",\"content\":\"Nothing specific\"},{\"role\":\"user\",\"content\":\"Here is the conversational responses data received from user: {\\\"key1\\\"=\\u003e[{\\\"id\\\"=\\u003e1, \\\"response\\\"=\\u003e\\\"response1\\\", \\\"prompt\\\"=\\u003e{\\\"pre_text\\\"=\\u003e\\\"pre1\\\", \\\"post_text\\\"=\\u003e\\\"post1\\\"}}, {\\\"id\\\"=\\u003e2, \\\"response\\\"=\\u003e\\\"response2\\\"}], \\\"key2\\\"=\\u003e[{\\\"id\\\"=\\u003e3, \\\"response\\\"=\\u003e\\\"response3\\\", \\\"prompt\\\"=\\u003e{\\\"pre_text\\\"=\\u003e\\\"pre3\\\", \\\"post_text\\\"=\\u003e\\\"post3\\\"}}]}\"}],\"temperature\":0.2}",
-      headers: {
-        'Accept'=>'*/*',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Content-Type'=>'application/json',
-        'User-Agent'=>'Ruby'
-      }).
-      to_return(status: 200, body: {
+    stub_request(:post, "https://api.openai.com/v1/chat/completions")
+      .with(
+        body: "{\"model\":\"gpt-3.5-turbo\",\"messages\":[{\"role\":\"system\",\"content\":\"Nothing specific\"},{\"role\":\"user\",\"content\":\"Here is the conversational responses data received from user: {\\\"key1\\\"=\\u003e[{\\\"id\\\"=\\u003e1, \\\"response\\\"=\\u003e\\\"response1\\\", \\\"prompt\\\"=\\u003e{\\\"pre_text\\\"=\\u003e\\\"pre1\\\", \\\"post_text\\\"=\\u003e\\\"post1\\\"}}, {\\\"id\\\"=\\u003e2, \\\"response\\\"=\\u003e\\\"response2\\\"}], \\\"key2\\\"=\\u003e[{\\\"id\\\"=\\u003e3, \\\"response\\\"=\\u003e\\\"response3\\\", \\\"prompt\\\"=\\u003e{\\\"pre_text\\\"=\\u003e\\\"pre3\\\", \\\"post_text\\\"=\\u003e\\\"post3\\\"}}]}\"}],\"temperature\":0.2}",
+        headers: {
+          "Accept" => "*/*",
+          "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+          "Content-Type" => "application/json",
+          "User-Agent" => "Ruby"
+        }
+      )
+      .to_return(status: 200, body: {
         choices: [
           {message: {content: stubbed_response}}
         ]
