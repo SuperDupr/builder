010008d629971281fc024325bed0c2b3f9ed2110
Moiz Ali <amoiz4142@gmail.com>
2023-11-02 18:17:36 +0500

    Add text parser method for dynamic manipulation

--- app/helpers/story_builders_helper.rb
+++ app/helpers/story_builders_helper.rb
@@ -1,2 +1,14 @@
 module StoryBuildersHelper
+  def ai_text_parser(text)
+    scanner = StringScanner.new
+    words = []
+    
+    while scanner.scan_until(/\{\{([^{}]+)\}\}/)
+      word = scanner[1]
+
+      words << word
+    end
+
+    words
+  end
 end
