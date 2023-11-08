d88c26b37defdaeb872a47ef5b3944017a9ce046
Moiz Ali <amoiz4142@gmail.com>
2023-11-08 13:15:31 +0500

    Add AI prompt based columns in questions

--- db/migrate/20231103113326_add_columns_to_questions.rb
+++ db/migrate/20231103113326_add_columns_to_questions.rb
@@ -3,5 +3,7 @@ class AddColumnsToQuestions < ActiveRecord::Migration[7.0]
     add_column :questions, :active, :boolean, default: true
     add_column :questions, :story_builder_id, :integer
     add_column :questions, :position, :integer
+    add_column :questions, :ai_prompt_attached, :boolean, default: false
+    add_column :questions, :ai_prompt, :text
   end
 end
