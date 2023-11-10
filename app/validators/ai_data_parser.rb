161c5eaeea4d502a5555ee2946be7092a3161401
Moiz Ali <amoiz4142@gmail.com>
2023-11-10 16:33:39 +0500

    Add helper classes in AiDataParser

--- app/validators/ai_data_parser.rb
+++ app/validators/ai_data_parser.rb
@@ -39,9 +39,81 @@
 # Find the answer finally
 
 class AiDataParser
-  attr_accessor :data
+  attr_accessor :data, :words
 
   def initialize(options = {})
     @data = options[:data]
+    @words = []
+  end
+
+  def parse
+    extract_words_and_add_answers
+    # level_one_validations
+  end  
+
+  private
+
+  def extract_words_and_add_answers
+    scanner = StringScanner.new(data)
+
+    while scanner.scan_until(/\{\{([^{}]+)\}\}/)
+      word = scanner[1]
+
+      # Run validations on word
+      validator = WordValidator.new(word: word).call
+
+      if validator[:success]
+        # Extract counter parts
+        counter_parts = ExtractAnswer.new(word: word).call if validator[:success]
+        # Find answer against the word
+        
+        # Reform the string
+        @data[scanner[1]] = answer
+      else
+        "Please answer according to your own mindset and information!"
+      end
+      # To be decided
+      # @words << word
+    end
+    # Finalize the original AI prompt associated with question
+    @data
+  end
+
+  # *Level #1: Validations*
+
+  # A valid choice must have *Q* alphabet
+  # A valid choice can't have alphabets except *Q*, *P*
+  # A valid choice must contain alphabets in order of [Q, P] or simply [Q]
+
+  # Q or P
+  class WordValidator
+    def initialize(word:)
+      @word = word
+    end
+
+    def call
+      # level_one_validations
+      # level_two_validations
+      { success: true, word: @word }
+    end
+
+    def level_one_validations
+    end
+    
+    def level_two_validations
+    end
+  end
+
+  class ExtractAnswer
+    def initialize(word:)
+      @word = word
+    end
+
+    def call
+      # Find question
+      # Find prompt if needed
+      # Find answer
+      { answer: @answer }
+    end
   end
 end
