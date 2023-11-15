31d8857c47533f7e6e1cc88e930f9a5ac5f932ff
Moiz Ali <amoiz4142@gmail.com>
2023-11-15 16:55:25 +0500

    Change selector column type in prompts

--- db/migrate/20230905110221_add_selector_to_prompts.rb
+++ db/migrate/20230905110221_add_selector_to_prompts.rb
@@ -1,5 +1,5 @@
 class AddSelectorToPrompts < ActiveRecord::Migration[7.0]
   def change
-    add_column :prompts, :selector, :string
+    add_column :prompts, :selector, :string, array: true, default: []
   end
 end
