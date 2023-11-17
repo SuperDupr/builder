da83ebeffe23c92c93967d192182f54616d79c9a
Moiz Ali <amoiz4142@gmail.com>
2023-11-18 00:51:28 +0500

    Add code to assign positions to prompts and answers

--- db/migrate/20231110113740_change_response_based_columns_types_of_answers_and_prompts.rb
+++ db/migrate/20231110113740_change_response_based_columns_types_of_answers_and_prompts.rb
@@ -4,6 +4,16 @@ class ChangeResponseBasedColumnsTypesOfAnswersAndPrompts < ActiveRecord::Migrati
     change_column :prompts, :selector, "varchar[] USING (string_to_array(selector, ','))"
     add_column :answers, :position, :integer
     add_column :prompts, :position, :integer
+
+    Question.all.each do |question|
+      question.answers.order(:updated_at).each.with_index(1) do |answer, index|
+        answer.update_column :position, index
+      end
+
+      question.prompts.order(:updated_at).each.with_index(1) do |prompt, index|
+        prompt.update_column :position, index
+      end
+    end
   end
   
   def down
