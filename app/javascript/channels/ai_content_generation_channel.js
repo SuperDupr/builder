a1237393f141e5e8d80743ab94eb47eb21216c82
Moiz Ali <amoiz4142@gmail.com>
2023-12-19 20:47:11 +0500

    Make a native answer field [Retake - Staging Bug] (#170)
    * Check presence of questionContent element
    
    * Make a native element declaration for answer field

--- app/javascript/channels/ai_content_generation_channel.js
+++ app/javascript/channels/ai_content_generation_channel.js
@@ -5,6 +5,7 @@ const questionContent = document.getElementById("questionContent")
 const contentBtn = document.getElementById("contentBtn")
 const nextQuestionButton = document.getElementById('questionForward');
 const aiContentDiv = document.getElementById("aiContentDiv")
+const answerFieldNative = document.getElementById("answer")
 
 if(spinnerElement) {
   spinnerElement.style.display = "none";
@@ -22,22 +23,28 @@ consumer.subscriptions.create("AiContentGenerationChannel", {
   },
 
   received(data) {
-    const answerField = document.getElementById("answer")
     // Called when there's incoming data on the websocket for this channel
     if(spinnerElement) {
       spinnerElement.style.display = "flex";
     }
-
+    
     console.log(data.body)
-
+    
     if (questionContent) {
       questionContent.style.display = "none";
     }
     
+    if (answerFieldNative) {
+      answerFieldNative.value = data.body
+      answerFieldNative.textContent = data.body
+    } else {
+      const answerField = document.getElementById("answer")
+      answerField.value = data.body
+      answerField.textContent = data.body
+    }
+    
     contentBtn.innerHTML = 'Create another version'
     nextQuestionButton.style.display = 'inline-flex'
-    answerField.value = data.body
-    answerField.textContent = data.body
     aiContentDiv.innerHTML = 
         `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
     
