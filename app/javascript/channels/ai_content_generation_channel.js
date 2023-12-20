d6c809bca0340b50b592a9fbcff481792240bd3d
Moiz Ali <amoiz4142@gmail.com>
2023-12-20 18:25:22 +0500

    A few additions for content generation bug

--- app/javascript/channels/ai_content_generation_channel.js
+++ app/javascript/channels/ai_content_generation_channel.js
@@ -1,14 +1,14 @@
 import consumer from "./consumer"
 
-const spinnerElement = document.querySelector(".spinnerStory")
-const questionContent = document.getElementById("questionContent")
-const contentBtn = document.getElementById("contentBtn")
-const nextQuestionButton = document.getElementById('questionForward');
-const aiContentDiv = document.getElementById("aiContentDiv")
+const spinnerElementNative = document.querySelector(".spinnerStory")
+const questionContentNative = document.getElementById("questionContent")
+const contentBtnNative = document.getElementById("contentBtn")
+const nextQuestionButtonNative = document.getElementById('questionForward');
+const aiContentDivNative = document.getElementById("aiContentDiv")
 const answerFieldNative = document.getElementById("answer")
 
-if(spinnerElement) {
-  spinnerElement.style.display = "none";
+if (spinnerElementNative) {
+  spinnerElementNative.style.display = "none";
 }
 
 consumer.subscriptions.create("AiContentGenerationChannel", {
@@ -24,14 +24,20 @@ consumer.subscriptions.create("AiContentGenerationChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    if(spinnerElement) {
-      spinnerElement.style.display = "flex";
+    if (spinnerElementNative) {
+      spinnerElementNative.style.display = "flex";
+    } else {
+      const spinnerElement = document.querySelector(".spinnerStory")
+      spinnerElement.style.display = "flex"
     }
     
     console.log(data.body)
     
-    if (questionContent) {
-      questionContent.style.display = "none";
+    if (questionContentNative) {
+      questionContentNative.style.display = "none";
+    } else {
+      const questionContent = document.getElementById("questionContent")
+      questionContent.style.display = "none"
     }
     
     if (answerFieldNative) {
@@ -42,15 +48,33 @@ consumer.subscriptions.create("AiContentGenerationChannel", {
       answerField.value = data.body
       answerField.textContent = data.body
     }
+
+    if (contentBtnNative) {
+      contentBtnNative.innerHTML = 'Create another version'
+    } else {
+      const contentBtn = document.getElementById("contentBtn")
+      contentBtn.innerHTML = "Create another version"
+    }
+
+    if (nextQuestionButtonNative) {
+      nextQuestionButtonNative.style.display = 'inline-flex'
+    } else {
+      const nextQuestionButton = document.getElementById('questionForward');
+      nextQuestionButton.style.display = 'inline-flex'
+    }
+
+    if (aiContentDivNative) {
+      aiContentDivNative.innerHTML = `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
+    } else {
+      const aiContentDiv = document.getElementById("aiContentDiv")
+      aiContentDiv.innerHTML = `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
+    }
     
-    contentBtn.innerHTML = 'Create another version'
-    nextQuestionButton.style.display = 'inline-flex'
-    aiContentDiv.innerHTML = 
-        `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
-    
-    
-    if(spinnerElement) {
-      spinnerElement.style.display = "none";
+    if (spinnerElementNative) {
+      spinnerElementNative.style.display = "none";
+    } else {
+      const spinnerElement2 = document.querySelector(".spinnerStory")
+      spinnerElement2.style.display = "none"
     }
   }
 });
