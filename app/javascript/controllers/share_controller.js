22ba7fde88ba6ef481eb2e686304db5beb6a0632
Imran Hayat <imran.hayat92@gmail.com>
2023-12-22 10:38:23 +0500

    Phase 5 UI Feedback (#178)

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -2,11 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 import TomSelect from "tom-select"
 
 export default class extends Controller {
-  static targets = ["shareInputField", "select"];
+  static targets = ["shareInputField", "select", "blogAccessAttached", "blogAccess"]
+
   connect() {
     this.selectSearch()
+
+    const blogAccessAttachedCheckBox = document.getElementById("blog_public_access")
+
+    if (!blogAccessAttachedCheckBox.checked) {
+      this.blogAccessTarget.style.display = "block"
+    }
+    else{
+      this.blogAccessTextareaTarget.required = true
+      blogAccessLabel.classList.remove('opacity-50')
+    }
   }
-  
+
+  toggleblogAccessContainerView(e) {
+    const blogAccessLabel = document.getElementById("blogAccessLabel")
+    if (e.target.checked) {
+      blogAccessLabel.classList.remove('opacity-50')
+      this.blogAccessTarget.style.display = "none"
+    } else {
+      blogAccessLabel.classList.add('opacity-50')
+      this.blogAccessTarget.style.display = "block"
+    }
+  }
 
   selectSearch() {
     const inputField = this.shareInputFieldTarget
