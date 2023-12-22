e6713d25b6f074bc597e04f7264acdb29674985a
Spencer Norman <spencerdnorman@gmail.com>
2023-12-22 17:16:36 -0600

    Ai content generation js fix (#176)
    * Live UI updates (#171)
    
    * made the cards on the dashboard dynamic
    
    * moved the question title, removed the status through prompts/questions
    
    * lint fix
    
    * removed the should navigate to a prompt test because we are no longer showing this unless there are multiple prompts, so the test needs to be updated which will be done later
    
    * Ai content generation js fix (#172)
    
    * Check presence of questionContent element
    
    * Make a native element declaration for answer field
    
    * Apply existence checks for elements
    
    ---------
    
    Co-authored-by: Moiz Ali <amoiz4142@gmail.com>
    
    * A few additions for content generation bug
    
    * UI Feedback of Phase #4 (#177)
    
    * phase-4-ui-feedback
    
    * Fix minor inconsistency
    
    * Remove log statements
    
    ---------
    
    Co-authored-by: Moiz Ali <amoiz4142@gmail.com>
    
    * Reset previous commits (#180)
    
    * Ai content generation js fix (#173)
    
    * Check presence of questionContent element
    
    * Make a native element declaration for answer field
    
    * Live UI updates (#171)
    
    * made the cards on the dashboard dynamic
    
    * moved the question title, removed the status through prompts/questions
    
    * lint fix
    
    * removed the should navigate to a prompt test because we are no longer showing this unless there are multiple prompts, so the test needs to be updated which will be done later
    
    * Apply existence checks for elements
    
    ---------
    
    Co-authored-by: Moiz Ali <amoiz4142@gmail.com>
    
    * putting the title back in the original spot
    
    * A few additions for content generation bug (#175)
    
    Co-authored-by: Moiz Ali <amoiz4142@gmail.com>
    
    ---------
    
    Co-authored-by: Moiz Ali <amoiz4142@gmail.com>
    
    ---------
    
    Co-authored-by: Moiz Ali <amoiz4142@gmail.com>
    Co-authored-by: Imran Hayat <imran.hayat92@gmail.com>

--- app/javascript/channels/ai_content_generation_channel.js
+++ app/javascript/channels/ai_content_generation_channel.js
@@ -5,7 +5,7 @@ const questionContentNative = document.getElementById("questionContent")
 const contentBtnNative = document.getElementById("contentBtn")
 const nextQuestionButtonNative = document.getElementById('questionForward');
 const aiContentDivNative = document.getElementById("aiContentDiv")
-const answerFieldNative = document.getElementById("answer")
+// const answerFieldNative = document.getElementById("answer")
 
 if (spinnerElementNative) {
   spinnerElementNative.style.display = "none";
@@ -14,12 +14,10 @@ if (spinnerElementNative) {
 consumer.subscriptions.create("AiContentGenerationChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
-    console.log("We are connected to AiContentGenerationChannel!")
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
-    console.log("We are disconnected to AiContentGenerationChannel!")
   },
 
   received(data) {
@@ -31,8 +29,6 @@ consumer.subscriptions.create("AiContentGenerationChannel", {
       spinnerElement.style.display = "flex"
     }
     
-    console.log(data.body)
-    
     if (questionContentNative) {
       questionContentNative.style.display = "none";
     } else {
@@ -40,13 +36,35 @@ consumer.subscriptions.create("AiContentGenerationChannel", {
       questionContent.style.display = "none"
     }
     
-    if (answerFieldNative) {
-      answerFieldNative.value = data.body
-      answerFieldNative.textContent = data.body
+    // if (answerFieldNative) {
+    const answerField = document.getElementById("answer")
+    answerField.value = data.body
+    answerField.textContent = data.body
+    // } else {
+    //   const answerField = document.getElementById("answer")
+    //   answerField.value = data.body
+    //   answerField.textContent = data.body
+    // }
+
+    if (contentBtnNative) {
+      contentBtnNative.innerHTML = 'Create another version'
     } else {
-      const answerField = document.getElementById("answer")
-      answerField.value = data.body
-      answerField.textContent = data.body
+      const contentBtn = document.getElementById("contentBtn")
+      contentBtn.innerHTML = "Create another version"
+    }
+
+    if (nextQuestionButtonNative) {
+      nextQuestionButtonNative.style.display = 'inline-flex'
+    } else {
+      const nextQuestionButton = document.getElementById('questionForward');
+      nextQuestionButton.style.display = 'inline-flex'
+    }
+
+    if (aiContentDivNative) {
+      aiContentDivNative.innerHTML = `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
+    } else {
+      const aiContentDiv = document.getElementById("aiContentDiv")
+      aiContentDiv.innerHTML = `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
     }
 
     if (contentBtnNative) {
