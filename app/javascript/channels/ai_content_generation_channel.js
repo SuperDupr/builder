7d283c875b48e355763b5efa5265e921b7dbbc7d
Spencer Norman <spencerdnorman@gmail.com>
2023-12-22 20:43:12 -0600

    getting things back to working

--- app/javascript/channels/ai_content_generation_channel.js
+++ app/javascript/channels/ai_content_generation_channel.js
@@ -1,3 +1,102 @@
+// import consumer from "./consumer"
+
+// const spinnerElementNative = document.querySelector(".spinnerStory")
+// const questionContentNative = document.getElementById("questionContent")
+// const contentBtnNative = document.getElementById("contentBtn")
+// const nextQuestionButtonNative = document.getElementById('questionForward');
+// const aiContentDivNative = document.getElementById("aiContentDiv")
+// // const answerFieldNative = document.getElementById("answer")
+
+// if (spinnerElementNative) {
+//   spinnerElementNative.style.display = "none";
+// }
+
+// consumer.subscriptions.create("AiContentGenerationChannel", {
+//   connected() {
+//     // Called when the subscription is ready for use on the server
+//   },
+
+//   disconnected() {
+//     // Called when the subscription has been terminated by the server
+//   },
+
+//   received(data) {
+//     // Called when there's incoming data on the websocket for this channel
+//     if (spinnerElementNative) {
+//       spinnerElementNative.style.display = "flex";
+//     } else {
+//       const spinnerElement = document.querySelector(".spinnerStory")
+//       spinnerElement.style.display = "flex"
+//     }
+    
+//     if (questionContentNative) {
+//       questionContentNative.style.display = "none";
+//     } else {
+//       const questionContent = document.getElementById("questionContent")
+//       questionContent.style.display = "none"
+//     }
+    
+//     // if (answerFieldNative) {
+//     const answerField = document.getElementById("answer")
+//     answerField.value = data.body
+//     answerField.textContent = data.body
+//     // } else {
+//     //   const answerField = document.getElementById("answer")
+//     //   answerField.value = data.body
+//     //   answerField.textContent = data.body
+//     // }
+
+//     if (contentBtnNative) {
+//       contentBtnNative.innerHTML = 'Create another version'
+//     } else {
+//       const contentBtn = document.getElementById("contentBtn")
+//       contentBtn.innerHTML = "Create another version"
+//     }
+
+//     if (nextQuestionButtonNative) {
+//       nextQuestionButtonNative.style.display = 'inline-flex'
+//     } else {
+//       const nextQuestionButton = document.getElementById('questionForward');
+//       nextQuestionButton.style.display = 'inline-flex'
+//     }
+
+//     if (aiContentDivNative) {
+//       aiContentDivNative.innerHTML = `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
+//     } else {
+//       const aiContentDiv = document.getElementById("aiContentDiv")
+//       aiContentDiv.innerHTML = `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
+//     }
+
+//     if (contentBtnNative) {
+//       contentBtnNative.innerHTML = 'Create another version'
+//     } else {
+//       const contentBtn = document.getElementById("contentBtn")
+//       contentBtn.innerHTML = "Create another version"
+//     }
+
+//     if (nextQuestionButtonNative) {
+//       nextQuestionButtonNative.style.display = 'inline-flex'
+//     } else {
+//       const nextQuestionButton = document.getElementById('questionForward');
+//       nextQuestionButton.style.display = 'inline-flex'
+//     }
+
+//     if (aiContentDivNative) {
+//       aiContentDivNative.innerHTML = `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
+//     } else {
+//       const aiContentDiv = document.getElementById("aiContentDiv")
+//       aiContentDiv.innerHTML = `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
+//     }
+    
+//     if (spinnerElementNative) {
+//       spinnerElementNative.style.display = "none";
+//     } else {
+//       const spinnerElement2 = document.querySelector(".spinnerStory")
+//       spinnerElement2.style.display = "none"
+//     }
+//   }
+// });
+
 import consumer from "./consumer"
 
 const spinnerElementNative = document.querySelector(".spinnerStory")
@@ -5,7 +104,7 @@ const questionContentNative = document.getElementById("questionContent")
 const contentBtnNative = document.getElementById("contentBtn")
 const nextQuestionButtonNative = document.getElementById('questionForward');
 const aiContentDivNative = document.getElementById("aiContentDiv")
-// const answerFieldNative = document.getElementById("answer")
+const answerFieldNative = document.getElementById("answer")
 
 if (spinnerElementNative) {
   spinnerElementNative.style.display = "none";
@@ -14,10 +113,12 @@ if (spinnerElementNative) {
 consumer.subscriptions.create("AiContentGenerationChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log("We are connected to AiContentGenerationChannel!")
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
+    console.log("We are disconnected to AiContentGenerationChannel!")
   },
 
   received(data) {
@@ -29,6 +130,8 @@ consumer.subscriptions.create("AiContentGenerationChannel", {
       spinnerElement.style.display = "flex"
     }
     
+    console.log(data.body)
+    
     if (questionContentNative) {
       questionContentNative.style.display = "none";
     } else {
@@ -36,35 +139,13 @@ consumer.subscriptions.create("AiContentGenerationChannel", {
       questionContent.style.display = "none"
     }
     
-    // if (answerFieldNative) {
-    const answerField = document.getElementById("answer")
-    answerField.value = data.body
-    answerField.textContent = data.body
-    // } else {
-    //   const answerField = document.getElementById("answer")
-    //   answerField.value = data.body
-    //   answerField.textContent = data.body
-    // }
-
-    if (contentBtnNative) {
-      contentBtnNative.innerHTML = 'Create another version'
+    if (answerFieldNative) {
+      answerFieldNative.value = data.body
+      answerFieldNative.textContent = data.body
     } else {
-      const contentBtn = document.getElementById("contentBtn")
-      contentBtn.innerHTML = "Create another version"
-    }
-
-    if (nextQuestionButtonNative) {
-      nextQuestionButtonNative.style.display = 'inline-flex'
-    } else {
-      const nextQuestionButton = document.getElementById('questionForward');
-      nextQuestionButton.style.display = 'inline-flex'
-    }
-
-    if (aiContentDivNative) {
-      aiContentDivNative.innerHTML = `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
-    } else {
-      const aiContentDiv = document.getElementById("aiContentDiv")
-      aiContentDiv.innerHTML = `<div class="contentDiv border p-3 rounded lg:w-2/3 xl:w-1/2">${data.body}</div>`
+      const answerField = document.getElementById("answer")
+      answerField.value = data.body
+      answerField.textContent = data.body
     }
 
     if (contentBtnNative) {
